e4807d26bada1f8c981697b473d0793751c0932b
Adriane Boyd <adrianeboyd@gmail.com>
2021-04-07 10:37:40 +0200

    v0.8.3: update to latest pytokenizations

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools_rust import RustExtension
 
 setup(
     name="spacy-alignments",
-    version="0.7.2",
+    version="0.8.3",
     packages=["spacy_alignments", "spacy_alignments.tests"],
     rust_extensions=[RustExtension("spacy_alignments.tokenizations")],
 )
