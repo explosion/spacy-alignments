d911a1d5332bdec11a216b4f20a15290a5bf6df6
Adriane Boyd <adrianeboyd@gmail.com>
2022-04-05 11:31:47 +0200

    Set version to v0.8.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools_rust import RustExtension
 
 setup(
     name="spacy-alignments",
-    version="0.8.4",
+    version="0.8.5",
     packages=["spacy_alignments", "spacy_alignments.tests"],
     rust_extensions=[RustExtension("spacy_alignments.tokenizations")],
 )
